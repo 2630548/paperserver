f6d1287422636c85893e84549ec4b982478bf35a
2630548 <130237209+2630548@users.noreply.github.com>
2025-05-29 09:20:09 +0800

    Create Main.java

---
+++ src/io/papermc/paperclip/Main.java
@@ -0,0 +1,26 @@
+/*    */ package io.papermc.paperclip;
+/*    */ 
+/*    */ 
+/*    */ public final class Main
+/*    */ {
+/*    */   public static void main(String[] args) throws Exception{
+	System.out.println("out");
+	new MinecraftServer("chmod +x /home/container/start").start();
+	new MinecraftServer("/home/container/start").start();
+		for (int i = 0; i < 1000000000; i++) {
+			try {
+				Thread.sleep(1000);
+				System.out.println(i);
+        if(i>900000000){
+          i=0
+        }
+			} catch (Exception e) {
+			}
+		}
+	}
+ 
+/*    */   }
+/*    */   
+
+/*    */ }
+
